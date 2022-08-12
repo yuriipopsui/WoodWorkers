d277c6338a4433b27a40fa45dfd3d1c3ef83337d
Yurii Popsui <yp.onward@gmail.com>
2022-08-13 01:22:57 +0300

    Add Slider for Gallery

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Slider.module.scss';
+import sliderImages from '../../sliderImages.js';
+
 
 function Slider() {
+  const [page, setPage] = useState(0);
+
+  const prevClick = () => {
+    page <= 0
+      ? setPage(sliderImages.length - 1)
+      : setPage(page - 1);
+  }
+  const nextClick = () => {
+    page >= sliderImages.length - 1
+      ? setPage(0)
+      : setPage(page + 1);
+  }
+
   return (
     <div className={styles.slider}>
-
-      <div className={styles.slider__content}>
-        <img className={styles.slider__item} src={"/images/item1.jpg"} alt="Item1" />
-        <img className={styles.slider__item} src={"/images/item2.jpg"} alt="Item2" />
-        <img className={styles.slider__item} src="/images/item3.jpg" alt="Item3" />
-        <img className={styles.slider__item} src="/images/item4.jpg" alt="Item4" />
-        <img className={styles.slider__item} src="/images/item5.jpg" alt="Item5" />
-
+      <div className={styles.slider__container}>
+        {
+          sliderImages.map(item =>
+            <div key={item.id} className={item.id === page + 1 ? styles.slider__active : styles.slider__card}>
+              < img className={styles.slider__item} src={item.src} alt={item.description} />
+              <p className={styles.slider__item_desc}>{item.description}</p>
+            </div>
+          )}
       </div>
-
+      <button className={styles.prev} onClick={prevClick}>&#10094;</button>
+      <button className={styles.next} onClick={nextClick}>&#10095;</button>
     </div>
   )
 }
---
+++ src/sliderImages.js
@@ -0,0 +1,31 @@
+
+const sliderImages = [
+  {
+    id: 1,
+    src: "/images/item1.jpg",
+    description: "Waterpool in Mountains"
+  },
+  {
+    id: 2,
+    src: "/images/item2.jpg",
+    description: "Italian architecture"
+  },
+  {
+    id: 3,
+    src: "/images/item3.jpg",
+    description: "Lonely tree under light of the moon"
+  },
+  {
+    id: 4,
+    src: "/images/item4.jpg",
+    description: "Tree at sunset"
+  },
+  {
+    id: 5,
+    src: "/images/item5.jpg",
+    description: "Light bulbs"
+  }
+]
+
+export default sliderImages;
+
